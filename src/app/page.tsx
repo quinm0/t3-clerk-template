f8d6c2a8290584e5835de724fa7bc974c0acddb1
KyleMoran138 <kylemoran.data@gmail.com>
2023-09-04 19:29:31 -0400

    delete everything

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next';
-
-const IndexPage: NextPage = () => {
-  return <div>IndexPage</div>;
-};
-
-export default IndexPage;
--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/(api|trpc)(.*)"],
-});
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
